6ad8efcbe55ac8af419e03647f0a5972b6947ab8
devpin95 <devpindroid@gmail.com>
2020-11-29 19:01:36 -0700

    reads in gene expression file

---
+++ Cluster.py
@@ -0,0 +1,28 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("exp_data", help="name of a file containing expression data")
+parser.add_argument("k", help="an integer indicating k, the number of clusters to form")
+args = parser.parse_args()
+
+exp_data = open(args.exp_data)
+k = int(args.k)
+
+print("k = " + str(k) + " on file " + args.exp_data)
+
+genes = []
+for line in exp_data:
+    gene = {}
+    gene_str = line.split('\t')
+
+    gene['id'] = gene_str[0]
+    gene['description'] = gene_str[1]
+
+    for i in range(0, len(gene_str[2:])):
+        if gene_str[i] == '':
+            gene_str[i] = '0.0'
+
+    gene['exp'] = [float(val) for val in gene_str[2:] if val != '']
+    print(gene)
+
+# print(genes)
